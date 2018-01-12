f7a796305088212639a895259851ed7723f358c7
Dominik Kalb <dominik.kalb@outlook.com>
2018-01-12 17:08:27 +0100

    stings.js will be filled later...

---
+++ functions/strings.js
@@ -0,0 +1,23 @@
+const categories = [
+    {
+      "category": "headquarters",
+      "suggestion": "Headquarters",
+      "facts": [
+        "Google's headquarters is in Mountain View, California.",
+        "Google has over 30 cafeterias in its main campus.",
+        "Google has over 10 fitness facilities in its main campus."
+      ],
+      "factPrefix": "Okay, here's a headquarters fact."
+    },
+    {
+      "category": "history",
+      "suggestion": "History",
+      "facts": [
+        "Google was founded in 1998.",
+        "Google was founded by Larry Page and Sergey Brin.",
+        "Google went public in 2004.",
+        "Google has more than 70 offices in more than 40 countries."
+      ],
+      "factPrefix": "Sure, here's a history fact."
+    }
+  ];
